b260e2fe574207a1e3c2948810ca110c28f6cd75
Alan Curtis <alan.curtis@bcfranchise.com>
2020-12-21 21:59:16 -0700

    delete fetch not being used

--- src/redux/models/fetch/fetchTypes.js
+++
@@ -1,4 +0,0 @@
-// Types: note types need to be unique for whole redux store
-export const FETCH_START = "FETCH_START";
-export const FETCH_SUCCESS = "FETCH_SUCCESS";
-export const FETCH_FAILURE = "FETCH_FAILURE";

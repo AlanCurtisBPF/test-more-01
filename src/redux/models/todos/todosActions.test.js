51b6e757c7195fa8add894d6f6aefc94961dc44f
Alan Curtis <alan.curtis@bcfranchise.com>
2020-12-22 18:04:45 -0700

    mock library for axios wip

--- src/redux/models/todos/todosActions.test.js
+++ src/redux/models/todos/todosActions.test.js
@@ -4,13 +4,16 @@ import thunk from "redux-thunk";
 import * as actions from "./todosActions";
 import * as types from "./todosTypes";
 
+import { initialState } from '../todos/todosReducer';
+import mockAxios from "axios";
+
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe("counterActions", () => {
   let store;
   beforeEach(() => {
-    store = mockStore({ counter: { count: 0 } });
+    store = mockStore(initialState);
   });
 
   it("test run", () => {
